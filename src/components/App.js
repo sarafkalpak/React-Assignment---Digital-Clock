f6d6187f8fddfba2c416cfd37fa4803f1a6207ac
sarafkalpak <sarafkalpak@gmail.com>
2021-01-19 18:26:29 +0530

    try3

--- src/components/App.js
+++ src/components/App.js
@@ -13,29 +13,45 @@ class App extends Component {
 // //     }
     constructor() {
         super();
-        this.state = { time: new Date() }; // initialise the state
+        this.state = { time: new Date() }; 
     }
 
-    componentDidMount() { // create the interval once component is mounted
+    componentDidMount() { 
         this.update = setInterval(() => {
             this.setState({ time: new Date() });
-        }, 1 * 1000); // every 1 seconds
+        }, 1 * 1000); 
     }
 
-    componentWillUnmount() { // delete the interval just before component is removed
+    componentWillUnmount() {
         clearInterval(this.update);
     }
 
-    render() {
-        const { time } = this.state; // retrieve the time from state
+    getTimeString() {
+        const currTime = this.state.time;
+        const [hours, minutes, seconds] = [currTime.getHours(), currTime.getMinutes(), currTime.getSeconds()];
 
-        return (<div className="Clock">
-            <h1>Digital Clock</h1>
-            <h2>
-                {/* print the string prettily */}
-                {time.toLocaleTimeString()}
-            </h2>
-        </div>);
+        const amOrPm = hours >=12 ? "PM" : "AM";
+        const twelveHourFormate = hours >= 12 ? hours-12 : hours;
+        const hoursString = '' + twelveHourFormate;
+        const minutesString = this.padNumberToTwoDigits(minutes);
+        const secondsString = this.padNumberToTwoDigits(seconds);
+
+        const timeString = `${hoursString }:${minutesString}:${secondsString} ${amOrPm}`;
+        return timeString;
+    }
+
+    padNumberToTwoDigits(num) {
+        // return (num < 10 ? ("0" + num) : ("" + num));
+        return `${num < 10 ? "0" : ""}${num}`   //templeate string
+
+    }
+
+    render() {
+        return (
+            <div className="Clock">
+                <h3 id="time">{this.getTimeString()}</h3>
+            </div>
+        );
     }
 }
 
