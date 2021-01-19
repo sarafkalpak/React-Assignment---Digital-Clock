d769d7285a6a5e0814c2cce61399f6addfbd4c6e
sarafkalpak <70629954+sarafkalpak@users.noreply.github.com>
2021-01-19 18:08:26 +0530

    Update App.js
    try2

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,68 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-//     render() {
+    constructor(props) {
+    super(props);
+    this.state = {
+      display: new Date().toLocaleTimeString()
+    };
+  }
+  componentDidMount() {
+    console.log("mounted");
+    this.intervalId = setInterval(
+      () => this.setState({ display: new Date().toLocaleTimeString() }),
+      1000
+    );
+  }
 
-//         return(
-//             <>
+ 
+
+  componentWillUnmount() {
+    console.log("unmounted");
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.display}</h3>
+      </div>
+    );
+  }
+}
+// //     render() {
+
+// //         return(
+// //             <>
                
-//             </>
-//         )
+// //             </>
+// //         )
+// //     }
+//     constructor() {
+//         super();
+//         this.state = { time: new Date() }; // initialise the state
 //     }
-    constructor() {
-        super();
-        this.state = { time: new Date() }; // initialise the state
-    }
 
-    componentDidMount() { // create the interval once component is mounted
-        this.update = setInterval(() => {
-            this.setState({ time: new Date() });
-        }, 1 * 1000); // every 1 seconds
-    }
+//     componentDidMount() { // create the interval once component is mounted
+//         this.update = setInterval(() => {
+//             this.setState({ time: new Date() });
+//         }, 1 * 1000); // every 1 seconds
+//     }
 
-    componentWillUnmount() { // delete the interval just before component is removed
-        clearInterval(this.update);
-    }
+//     componentWillUnmount() { // delete the interval just before component is removed
+//         clearInterval(this.update);
+//     }
 
-    render() {
-        const { time } = this.state; // retrieve the time from state
+//     render() {
+//         const { time } = this.state; // retrieve the time from state
 
-        return (<div className="Clock">
-            <h1>Digital Clock</h1>
-            <h2>
-                {/* print the string prettily */}
-                {time.toLocaleTimeString()}
-            </h2>
-        </div>);
-    }
+//         return (<div className="Clock">
+//             <h1>Digital Clock</h1>
+//             <h2>
+//                 {/* print the string prettily */}
+//                 {time.toLocaleTimeString()}
+//             </h2>
+//         </div>);
+//     }
 }
 
 
